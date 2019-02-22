c3a937fd79c72ade16c5bd00bca7929ec6c2ad71
Angela <Angela.ohh@gmail.com>
2019-02-22 12:46:33 -0800

    modification practice

--- hello.rb
+++ hello.rb
@@ -1,3 +1,8 @@
+require "colorize"
+
 puts "Hello World!"
 puts "Hello world is a traditional for programmers to test first input."
-puts "Testing another change."
+puts "Testing another change.".colorize(:green).blink
+
+puts "This is blue".colorize(:red).underline
+puts String.modes
